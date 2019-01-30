eddb52452b7798bb11a202f19655e65d5f274220
HiroyaMasuko <c18015@std.it-college.ac.jp>
2019-01-30 16:13:46 +0900

    プラットフォームをAndroidに変え、デバック用のステージと前データのプログラムとプレファブを持ってきた。

---
+++ Gaikare_2019_Speed/Assets/Program/Camera_Controller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{                                                       //なかったことになったカメラジャイロ
+
+    float Right_maxRotate = 20;//回転角の最大値
+    float Left_maxRotate = 20;
+    float tmpRotate = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        //Gyro.x = 0.01f; // Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float C = Input.GetAxis("Vertical");
+        //float v = Input.acceleration.x; // Input.acceleration.y;
+        //float C = Input.acceleration.x
+
+        if (v >= 0.51f)
+        {
+            v = 0.5f;
+        }
+
+        if (v <= -0.51f)
+        {
+            v = -0.5f;
+        }
+         transform.localRotation *= Quaternion.Euler(0, 0, -C / 2f);
+
+        
+        if (C >= 0.9f)
+        {
+            Debug.Log("migi");
+            
+            this.transform.rotation *= Quaternion.Euler(0, 0, C);
+
+
+
+        }
+
+
+        if (C <= -0.9f)
+        {
+            Debug.Log("hidari");
+            
+            this.transform.rotation *= Quaternion.Euler(0, 0, C);
+
+        }
+
+        Debug.Log(C);
+        
+    }
+}
---
+++ Gaikare_2019_Speed/Assets/Program/Enemy_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour {
+
+    public float Speed;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.Translate(0, 0, 1 * Speed * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Baria")
+        {
+          
+
+            Destroy(this.gameObject);
+        }
+    }
+
+}
