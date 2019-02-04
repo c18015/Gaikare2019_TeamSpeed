c88d9e0ce4feef9cba7516a3600d51ce828bbf6d
c18008 <c18008@std.it-college.ac.jp>
2019-02-04 16:30:53 +0900

    リザルト移管（成功・失敗）のシーンを計４種追加 目的地への到着時間（アイスメーターの残り）によって、各リザルトに遷移するプログラムを作成

---
+++ Gaikare_2019_Speed/Assets/Program/ResultController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultController : MonoBehaviour {
+
+    float time;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+    void Update ()
+    {
+        time += Time.deltaTime;
+
+    Vector3 pos = transform.position;
+        transform.position = pos;
+
+        if (pos.z >= 180)
+        {
+            if (time < 10)
+            {
+                SceneManager.LoadScene("GameClear(3stars)");
+            }
+            if (time >= 10)
+            {
+                SceneManager.LoadScene("GameClear(2stars)");
+            }
+            if (time >= 15)
+            {
+                SceneManager.LoadScene("GameClear(1star)");
+            }
+
+        }
+
+        if (pos.z < 180)
+        {
+            if (time >= 20)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+
+        }
+        
+    }
+
+
+}
